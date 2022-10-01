82cb3621ee74012193d802d8a59c075c2ed35a3a
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-10-02 00:39:32 +0530

    Update n-ary-tree-level-order-traversal.cpp

--- n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.cpp
+++ n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.cpp
@@ -1,23 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-    Node() {}
-    Node(int _val) {
-        val = _val;
-    }
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 // Definition for a n-ary tree node.
 
 /**
